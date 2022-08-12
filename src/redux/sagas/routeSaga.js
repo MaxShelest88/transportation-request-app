bec3862a5f37f870bab3bdcc533bec0b948ba375
Maksim Shelest <shelest.ks@gmail.com>
2022-08-12 11:20:10 +0300

    try catch

--- src/redux/sagas/routeSaga.js
+++ src/redux/sagas/routeSaga.js
@@ -1,17 +1,21 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
-import { getRouteSuccess } from '../route/slice';
+import { getRouteSuccess, getRouteError } from '../route/slice';
 
 function* getRouteFetchWorker(action) {
   const { startPoint, finishPoint } = action.payload;
-  const { data } = yield call(() =>
-    axios.get(
-      `https://api.geoapify.com/v1/routing?waypoints=${startPoint.position.join(
-        ',',
-      )}|${finishPoint.position.join(',')}&mode=drive&apiKey=${process.env.REACT_APP_API_KEY}`,
-    ),
-  );
-  yield put(getRouteSuccess(data.features));
+  try {
+    const { data } = yield call(() =>
+      axios.get(
+        `https://api.geoapify.com/v1/routing?waypoints=${startPoint.position.join(
+          ',',
+        )}|${finishPoint.position.join(',')}&mode=drive&apiKey=${process.env.REACT_APP_API_KEY}`,
+      ),
+    );
+    yield put(getRouteSuccess(data.features));
+  } catch (err) {
+    put(getRouteError());
+  }
 }
 
 function* routeSaga() {
