8dbc7231160598e3a28a685a80b50e5eff455dde
Maksim Shelest <shelest.ks@gmail.com>
2022-08-12 11:31:09 +0300

    styles

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { MapContainer, TileLayer, useMap, Popup, Marker,} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import GeojsonLayer from '../GeojsonLayer/GeojsonLayer';
-
+import classes from './MapComponent.module.scss'
 
 const MapComponent = () => {
   const position = [55.702868, 38.530865];
   return (
-    <MapContainer center={position} zoom={7} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <GeojsonLayer />
-    </MapContainer>
+    <div className={classes.root}>
+    	<MapContainer className={classes.map} center={position} zoom={7} scrollWheelZoom={false}>
+	      <TileLayer
+	        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+	      />
+	      <GeojsonLayer />
+	    </MapContainer>
+    </div>
   );
 };
 
